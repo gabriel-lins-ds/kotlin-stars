9ce2879bfd7739876f0f061891d048f069ddd2fb
Gabriel Lins <gabriel.linss@hotmail.com>
2026-03-23 17:28:40 -0300

    Fixes constants location + removes unused drawable/values (#5)

--- app/src/main/java/com/glins/android/apps/ui/constants/UiConstants.kt
+++ app/src/main/java/com/glins/android/apps/ui/constants/UiConstants.kt
@@ -1,10 +1,14 @@
 package com.glins.android.apps.ui.constants
 
-import com.glins.android.apps.data.constants.NetworkConstants.NETWORK_PAGE_SIZE
 
 object UiConstants {
+
+    private const val NETWORK_PAGE_SIZE = 100
     const val FAB_VISIBILITY_THRESHOLD = NETWORK_PAGE_SIZE / 4
     const val AVATAR_SIZE = 48
     const val AVATAR_SIZE_DETAILS = 96
     const val SCREEN_TRANSITION_ANIMATION_DURATION = 500
+    const val GITHUB_URL_AVATAR_SIZE_SUFFIX = "&s=${AVATAR_SIZE}"
+    const val GITHUB_URL_AVATAR_SIZE_DETAILS_SUFFIX = "&s=${AVATAR_SIZE_DETAILS}"
+    const val REFRESH_TIME_THRESHOLD = 10 * 1000L // 10 seconds
 }
