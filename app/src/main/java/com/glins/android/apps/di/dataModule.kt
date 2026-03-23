53511a9b16859e192c075ec64db708723f90d246
gabriel-lins-ds <gabriel.linss@hotmail.com>
2026-03-23 19:10:21 -0300

    Updates DI files for a cleaner readability + Adds BuildConfig fields

--- app/src/main/java/com/glins/android/apps/di/dataModule.kt
+++ app/src/main/java/com/glins/android/apps/di/dataModule.kt
@@ -3,21 +3,11 @@ package com.glins.android.apps.di
 import com.glins.android.apps.data.local.KotlinStarsLocalDataSource
 import com.glins.android.apps.data.repository.KotlinStarsRepositoryImpl
 import com.glins.android.apps.domain.repository.KotlinStarsRepository
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val dataModule = module {
-    single {
-        KotlinStarsLocalDataSource(
-            repositoryDao = get(),
-            remoteKeysDao = get(),
-            appDatabase = get()
-        )
-    }
-
-    single<KotlinStarsRepository> {
-        KotlinStarsRepositoryImpl(
-            localDataSource = get(),
-            api = get()
-        )
-    }
+    singleOf(::KotlinStarsLocalDataSource)
+    singleOf(::KotlinStarsRepositoryImpl) { bind<KotlinStarsRepository>() }
 }
--- app/src/main/java/com/glins/android/apps/di/viewModelModule.kt
+++ app/src/main/java/com/glins/android/apps/di/viewModelModule.kt
@@ -3,19 +3,10 @@ package com.glins.android.apps.di
 import com.glins.android.apps.ui.repositorydetails.RepositoryDetailsViewModel
 import com.glins.android.apps.ui.repositorylist.RepositoryListViewModel
 import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel {
-        RepositoryListViewModel(
-            repository = get()
-        )
-    }
-
-    viewModel {
-        RepositoryDetailsViewModel(
-            savedStateHandle = get(),
-            repository = get()
-        )
-    }
+    viewModelOf(::RepositoryListViewModel)
+    viewModelOf(::RepositoryDetailsViewModel)
 }
