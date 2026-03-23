55ceadfd3a12b1d6d3295bd93cb0cc3000cc222e
Gabriel Lins <gabriel.linss@hotmail.com>
2026-03-23 19:14:47 -0300

    feature(di): Dependency Injection Readability (#6)

--- app/src/main/java/com/glins/android/apps/di/databaseModule.kt
+++ app/src/main/java/com/glins/android/apps/di/databaseModule.kt
@@ -1,26 +1,23 @@
 package com.glins.android.apps.di
 
 import androidx.room.Room
-import com.glins.android.apps.data.local.dao.RemoteKeysDao
-import com.glins.android.apps.data.local.dao.RepositoryDao
 import com.glins.android.apps.data.local.database.AppDatabase
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
+private const val DATABASE_NAME = "repositories.db"
 val databaseModule = module {
     single {
         Room.databaseBuilder(
             androidContext(),
             AppDatabase::class.java,
-            "repositories.db"
-        ).build()
+            DATABASE_NAME
+        )
+            .fallbackToDestructiveMigration(dropAllTables = true)
+            .build()
     }
 
-    single<RepositoryDao> {
-        get<AppDatabase>().repositoryDao()
-    }
+    single { get<AppDatabase>().repositoryDao() }
 
-    single<RemoteKeysDao> {
-        get<AppDatabase>().remoteKeysDao()
-    }
+    single { get<AppDatabase>().remoteKeysDao() }
 }
