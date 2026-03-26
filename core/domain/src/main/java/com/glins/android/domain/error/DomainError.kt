461d96e05e7be0abb7b7d273ffdfd668bba816c3
gabriel-lins-ds <gabriel.linss@hotmail.com>
2026-03-26 02:35:22 -0300

    Makes GithubSearchLimit error non-retryable

--- core/domain/src/main/java/com/glins/android/domain/error/DomainError.kt
+++ core/domain/src/main/java/com/glins/android/domain/error/DomainError.kt
@@ -1,9 +1,9 @@
 package com.glins.android.domain.error
 
-sealed class DomainError {
+sealed class DomainError(val isRetryable: Boolean = true) {
     data object Network : DomainError()
     data object GithubRequestLimit : DomainError()
-    data object GithubSearchLimit : DomainError()
+    data object GithubSearchLimit : DomainError(isRetryable = false)
     data object Server : DomainError()
     data object NotFound : DomainError()
     data object Unknown : DomainError()
